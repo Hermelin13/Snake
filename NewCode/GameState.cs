31c38f9f692dfc7948654e1a1711de178f9bdc47
Hermelin <adamdaliborj@gmail.com>
2026-02-17 23:38:48 +0100

    adding game state and snake

---
+++ NewCode/GameState.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Snake;
+
+class GameState
+{
+    public int Score { get; set; }
+    public bool IsGameOver { get; set; }
+    public const int InitialScore = 5;
+
+    public GameState()
+    {
+        Score = InitialScore;
+        IsGameOver = false;
+    }
+
+    public void EatFood()
+    {
+        Score++;
+    }
+
+    public void EndGame()
+    {
+        IsGameOver = true;
+    }
+}
---
+++ NewCode/Snake.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Snake;
+
+class Snake
+{
+    public Position Head { get; set; }
+    public List<Position> Body { get; private set; }
+    public ConsoleColor Color { get; private set; }
+
+    public Snake(int screenWidth, int screenHeight)
+    {
+        Head = new Position(screenWidth / 2, screenHeight / 2);
+        Body = new List<Position>();
+        Color = ConsoleColor.Red;
+    }
+
+    public void Move(Direction direction)
+    {
+        // Automaically grow the body by adding the current head position to the body list
+        Body.Insert(0, new Position(Head.X, Head.Y));
+
+        // Move the head in the specified direction
+        switch (direction)
+        {
+            case Direction.Up:
+                Head.Y--;
+                break;
+            case Direction.Down:
+                Head.Y++;
+                break;
+            case Direction.Left:
+                Head.X--;
+                break;
+            case Direction.Right:
+                Head.X++;
+                break;
+        }
+    }
+
+    public void TrimBody(int maxLength)
+    {
+        while (Body.Count >= maxLength)
+            Body.RemoveAt(Body.Count - 1);
+    }
+
+    public bool CheckSelfCollision()
+    {
+        return Body.Any(segment => segment.X == Head.X && segment.Y == Head.Y);
+    }
+}
