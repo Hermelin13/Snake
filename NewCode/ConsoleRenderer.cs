cfce7e8d28bf247f5d64404cdc2b46ada3f679eb
Hermelin <adamdaliborj@gmail.com>
2026-02-20 08:43:55 +0100

    console setup

--- NewCode/ConsoleRenderer.cs
+++ NewCode/ConsoleRenderer.cs
@@ -26,5 +26,32 @@ public class ConsoleRenderer
     {
         Console.Clear();
     }
+    
+    public void DrawBorders()
+    {
+        // top and bottom borders
+        DrawHorizontalLine(0);
+        DrawHorizontalLine(screenHeight - 1);
+
+        // left and right borders
+        DrawVerticalLine(0);
+        DrawVerticalLine(screenWidth - 1);
+    }
+
+    private void DrawHorizontalLine(int y)
+    {
+        Console.SetCursorPosition(0, y);
+        for (int x = 0; x < screenWidth; x++)
+            Console.Write(GameConfig.BORDER_CHAR);
+    }
+
+    private void DrawVerticalLine(int x)
+    {
+        for (int y = 0; y < screenHeight; y++)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(GameConfig.BORDER_CHAR);
+        }
+    }
 
 }
